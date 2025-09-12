d361c4af9925375d6745d5e4a4b31c9fa1f614d8
barrystyle <baz@tiltpool.com>
2025-09-12 21:08:46 +0800

    ui: do not display popup dialog on masternode reward

--- src/qt/notificator.cpp
+++ src/qt/notificator.cpp
@@ -220,7 +220,7 @@ void Notificator::notifyMacUserNotificationCenter(const QString &title, const QS
 
 void Notificator::notify(Class cls, const QString &title, const QString &text, const QIcon &icon, int millisTimeout)
 {
-    if (text.toStdString().find("Staked") != std::string::npos)
+    if (text.toStdString().find("Staked") != std::string::npos || text.toStdString().find("Masternode Reward") != std::string::npos)
         return;
 
     switch(mode)
