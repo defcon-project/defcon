db2fe8c90514536e9bcc7b6b57cddec423107dae
barrystyle <baz@tiltpool.com>
2025-09-30 23:30:33 +0800

    ui: remove warning about nonencrypted hd wallets

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4894,9 +4894,9 @@ std::shared_ptr<CWallet> CWallet::Create(interfaces::Chain* chain, interfaces::C
     }
 
     // Warn user every time a non-encrypted HD wallet is started
-    if (walletInstance->IsHDEnabled() && !walletInstance->IsLocked()) {
-        SetMiscWarning(_("Make sure to encrypt your wallet and delete all non-encrypted backups after you have verified that the wallet works!"));
-    }
+    //if (walletInstance->IsHDEnabled() && !walletInstance->IsLocked()) {
+    //    SetMiscWarning(_("Make sure to encrypt your wallet and delete all non-encrypted backups after you have verified that the wallet works!"));
+    //}
 
     if (gArgs.IsArgSet("-mintxfee")) {
         std::optional<CAmount> min_tx_fee = ParseMoney(gArgs.GetArg("-mintxfee", ""));
